31dc6bf38fa958ad1a9b7d9b1e0686688b4ce603
Josephine Pfeiffer <hi@josie.lol>
2026-01-17 21:27:11 +0100

    refactor: extract shared constants and debounce hooks
    - Create src/constants.ts with PER_PAGE_OPTIONS and SEARCH_DEBOUNCE_MS
    - Create src/hooks/useDebounce.ts with useDebouncedValue and
      useDebouncedCallback hooks for future use
    - Update PackageList and SearchView to use shared constants
    
    Signed-off-by: Josephine Pfeiffer <hi@josie.lol>

--- src/components/PackageList.tsx
+++ src/components/PackageList.tsx
@@ -31,14 +31,7 @@ import {
 } from "../api";
 import { sanitizeSearchInput } from "../utils";
 import { PackageDetailsModal } from "./PackageDetailsModal";
-
-const PER_PAGE_OPTIONS = [
-  { title: "20", value: 20 },
-  { title: "50", value: 50 },
-  { title: "100", value: 100 },
-];
-
-const SEARCH_DEBOUNCE_MS = 300;
+import { PER_PAGE_OPTIONS, SEARCH_DEBOUNCE_MS } from "../constants";
 
 export const PackageList: React.FC = () => {
   const [packages, setPackages] = useState<Package[]>([]);
--- src/components/SearchView.tsx
+++ src/components/SearchView.tsx
@@ -28,14 +28,9 @@ import { Table, Thead, Tr, Th, Tbody, Td, ThProps } from "@patternfly/react-tabl
 import { SearchResult, SyncPackageDetails, searchPackages, getSyncPackageInfo, InstalledFilterType } from "../api";
 import { sanitizeSearchInput } from "../utils";
 import { PackageDetailsModal } from "./PackageDetailsModal";
+import { PER_PAGE_OPTIONS, SEARCH_DEBOUNCE_MS } from "../constants";
 
 const MIN_SEARCH_LENGTH = 1;
-const SEARCH_DEBOUNCE_MS = 300;
-const PER_PAGE_OPTIONS = [
-  { title: "20", value: 20 },
-  { title: "50", value: 50 },
-  { title: "100", value: 100 },
-];
 
 export const SearchView: React.FC = () => {
   const [results, setResults] = useState<SearchResult[]>([]);
